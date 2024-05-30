3ada2ca4681556dee38c302c118fbfaf69c58727
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-05-30 22:41:17 +0700

    Tạo các router con

---
+++ routes/client/product.route.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.render("client/pages/products/index");
+});
+
+// Ví dụ
+// router.post("/create", (req, res) => {
+//   res.render("client/pages/products/create");
+// });
+
+// router.patch("/edit", (req, res) => {
+//   res.render("client/pages/products/edit");
+// });
+
+// router.get("/detail", (req, res) => {
+//   res.render("client/pages/products/detail");
+// });
+
+module.exports = router;
