354fedbf9bc13b67f73a035531f3c6b01b531ff0
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-06-13 21:42:01 +0700

    3.1. Thay đổi trạng thái một sản phẩm - Gửi lên bằng phương thức PATCH

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -64,3 +64,26 @@ if(listButtonPagination.length > 0) {
   });
 }
 // End Pagination
+
+// Button Change Status
+const listButtonChangeStatus = document.querySelectorAll("[button-change-status]");
+if(listButtonChangeStatus.length > 0) {
+  listButtonChangeStatus.forEach(button => {
+    button.addEventListener("click", () => {
+      const link = button.getAttribute("link");
+      fetch(link, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then(res => res.json())
+        .then(data => {
+          if(data.code == 200) {
+            window.location.reload();
+          }
+        })
+    });
+  });
+}
+// End Button Change Status
