1d344a62d039c5d3143c1c73935b528cf94b9a16
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-06-29 21:25:01 +0700

    4. Làm trang chi tiết sản phẩm bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -22,3 +22,25 @@ module.exports.index = async (req, res) => {
     products: products
   });
 }
+
+// [GET] /products/:slug
+module.exports.detail = async (req, res) => {
+  const slug = req.params.slug;
+
+  const product = await Product.findOne({
+    slug: slug,
+    deleted: false,
+    status: "active"
+  });
+
+  console.log(product);
+
+  if(product) {
+    res.render("client/pages/products/detail", {
+      pageTitle: "Chi tiết sản phẩm",
+      product: product
+    });
+  } else {
+    res.redirect("/");
+  }
+}
