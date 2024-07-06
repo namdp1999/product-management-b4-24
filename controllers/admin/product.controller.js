56abfcff17628acfb8e425e043c29643ad03fe9e
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-07-06 23:08:55 +0700

    2.2. Phần chỉnh sửa sản phẩm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -193,9 +193,16 @@ module.exports.edit = async (req, res) => {
     });
 
     if(product) {
+      const categories = await ProductCategory.find({
+        deleted: false
+      });
+    
+      const newCategories = createTreeHelper(categories);
+
       res.render("admin/pages/products/edit", {
         pageTitle: "Chỉnh sửa sản phẩm",
-        product: product
+        product: product,
+        categories: newCategories
       });
     } else {
       res.redirect(`/${systemConfig.prefixAdmin}/products`);
