a18cbd877499caf3ea4121d4d25bf6715f52b452
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-07-20 22:30:03 +0700

    2.2. Làm nút Mini Cart trên Header

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -13,6 +13,11 @@ module.exports.cartId = async (req, res, next) => {
       { 
         expires: new Date(Date.now() + expires)
       });
+  } else {
+    const cart = await Cart.findOne({
+      _id: req.cookies.cartId
+    });
+    res.locals.cartTotal = cart.products.length || 0;
   }
   
   next();
