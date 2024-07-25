51192abb6fb4c1a3bff320a382a8c482edc98f7b
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-07-25 22:52:13 +0700

    3. Quên mật khẩu - Nhập mã OTP

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -123,3 +123,45 @@ module.exports.forgotPasswordPost = async (req, res) => {
 
   res.redirect(`/user/password/otp?email=${email}`);
 };
+
+// [GET] /user/password/otp
+module.exports.otpPassword = async (req, res) => {
+  const email = req.query.email;
+
+  res.render("client/pages/user/otp-password", {
+    pageTitle: "Xác thực OTP",
+    email: email
+  });
+};
+
+// [POST] /user/password/otp
+module.exports.otpPasswordPost = async (req, res) => {
+  const email = req.body.email;
+  const otp = req.body.otp;
+
+  const result = await ForgotPassword.findOne({
+    email: email,
+    otp: otp
+  });
+
+  if(!result) {
+    req.flash("error", "OTP không hợp lệ!");
+    res.redirect("back");
+    return;
+  }
+
+  const user = await User.findOne({
+    email: email
+  });
+
+  res.cookie("tokenUser", user.tokenUser);
+
+  res.redirect("/user/password/reset");
+};
+
+// [GET] /user/password/reset
+module.exports.resetPassword = async (req, res) => {
+  res.render("client/pages/user/reset-password", {
+    pageTitle: "Đổi mật khẩu mới"
+  });
+};
