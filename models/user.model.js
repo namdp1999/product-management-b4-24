a8ca15f99c14578513354d83a8f8037e261e522b
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-08-15 22:44:05 +0700

    1.4 + 1.5 + 1.6

--- models/user.model.js
+++ models/user.model.js
@@ -16,7 +16,8 @@ const userSchema = new mongoose.Schema({
     default: false
   },
   acceptFriends: Array,
-  requestFriends: Array
+  requestFriends: Array,
+  friendsList: Array
 }, {
   timestamps: true
 });
