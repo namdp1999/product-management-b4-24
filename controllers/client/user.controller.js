35fd6ba8af243a2d75351a5f1bc06975ae162e22
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-08-17 23:01:52 +0700

    2.2. Hiển thị realtime

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -83,6 +83,14 @@ module.exports.loginPost = async (req, res) => {
     statusOnline: "online"
   });
 
+  _io.once("connection", (socket) => {
+    // Trả ra cho bạn bè trạng thái online của A
+    socket.broadcast.emit("SERVER_RETURN_USER_ONLINE", {
+      status: "online",
+      userIdA: user.id
+    })
+  });
+
   req.flash("success", "Đăng nhập thành công!");
   res.redirect("/");
 };
@@ -98,6 +106,14 @@ module.exports.logout = async (req, res) => {
   } catch(e) {
     console.log(e);
   }
+
+  _io.once("connection", (socket) => {
+    // Trả ra cho bạn bè trạng thái offline của A
+    socket.broadcast.emit("SERVER_RETURN_USER_ONLINE", {
+      status: "offline",
+      userIdA: res.locals.user.id
+    })
+  });
 
   res.clearCookie("tokenUser");
   res.redirect("/user/login");
