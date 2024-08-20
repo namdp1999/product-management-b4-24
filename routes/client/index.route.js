5dc13775ac9ece39a2cabd22ccd1feafe17ef6b4
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-08-20 22:36:42 +0700

    2.1. Làm phần Giao diện danh sách phòng chat

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -6,6 +6,7 @@ const checkoutRoute = require("./checkout.route");
 const userRoute = require("./user.route");
 const chatRoute = require("./chat.route");
 const usersRoute = require("./users.route");
+const roomsChatRoute = require("./rooms-chat.route");
 
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
 const cartMiddleware = require("../../middlewares/client/cart.middleware");
@@ -40,5 +41,11 @@ module.exports.index = (app) => {
     "/users",
     userMiddleware.requireAuth,
     usersRoute
+  );
+
+  app.use(
+    "/rooms-chat",
+    userMiddleware.requireAuth,
+    roomsChatRoute
   );
 }
